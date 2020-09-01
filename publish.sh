b340bcc19e2f005c5411eacd0cb213e1f80018d8
Tony Duco <nosduco@gmail.com>
2020-09-01 13:34:00 -0400

    Fix case sensitive pwd

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 npm run-script generate:schema
-apollo service:push --graph=$APOLLO_GRAPH --variant=$APOLLO_GRAPH_VARIANT --serviceName=$APOLLO_SERVICE_NAME --serviceURL=$APOLLO_SERVICE_URL --localSchemaFile=$(PWD)/schema.gql
+apollo service:push --graph=$APOLLO_GRAPH --variant=$APOLLO_GRAPH_VARIANT --serviceName=$APOLLO_SERVICE_NAME --serviceURL=$APOLLO_SERVICE_URL --localSchemaFile=$(pwd)/schema.gql
